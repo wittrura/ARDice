3741dece20f7a5f8df1c6d5f5a1f2732de655570
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-12-02 05:05:18 -0600

    feat: adds ability to delete all dice, cleans and refactors code

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        // self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         
         // Set the view's delegate
         sceneView.delegate = self
@@ -73,43 +73,40 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.run(configuration)
     }
     
+    //MARK: - Dice Rendering Methods
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
             let touchLocation = touch.location(in: sceneView)
             
             let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
             
-            if let hitResults = results.first {
-                // Create a new scene
-                //let scene = SCNScene(named: "art.scnassets/ship.scn")!
-                let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
-
-                // if let block allows checking for presence of diceNode before adding to sceneView
-                if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
-                    // set the position of the node based off the real-world dimensions from the hitResult
-                    diceNode.position = SCNVector3(
-                        x: hitResults.worldTransform.columns.3.x,
-                        y: hitResults.worldTransform.columns.3.y + diceNode.boundingSphere.radius, // to lay flat rather than centered
-                        z: hitResults.worldTransform.columns.3.z
-                    )
-                    
-                    diceArray.append(diceNode)
-                    sceneView.scene.rootNode.addChildNode(diceNode)
-                    
-                    roll(dice: diceNode)
-                }
+            if let hitResult = results.first {
+                addDice(atLocation: hitResult)
             }
         }
     }
     
-    
-    func rollAllDice() -> Void {
-        if !diceArray.isEmpty {
-            for dice in diceArray {
-                roll(dice: dice)
-            }
+    func addDice(atLocation location: ARHitTestResult) -> Void {
+        // Create a new scene
+        //let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+        
+        // if let block allows checking for presence of diceNode before adding to sceneView
+        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+            // set the position of the node based off the real-world dimensions from the hitResult
+            diceNode.position = SCNVector3(
+                x: location.worldTransform.columns.3.x,
+                y: location.worldTransform.columns.3.y + diceNode.boundingSphere.radius, // to lay flat rather than centered
+                z: location.worldTransform.columns.3.z
+            )
+            
+            diceArray.append(diceNode)
+            sceneView.scene.rootNode.addChildNode(diceNode)
+            
+            roll(dice: diceNode)
         }
     }
+    
     
     func roll(dice: SCNNode) -> Void {
         let randomX = Float((arc4random_uniform(4) + 1)) * (Float.pi / 2)
@@ -123,6 +120,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         )
     }
     
+    func rollAllDice() -> Void {
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                roll(dice: dice)
+            }
+        }
+    }
+    
     
     @IBAction func rollAgain(_ sender: UIBarButtonItem) {
         rollAllDice()
@@ -133,31 +138,50 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         print("shook phone")
     }
     
-    
-    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        if anchor is ARPlaneAnchor {
-            
-            let planeAnchor = anchor as! ARPlaneAnchor //down cast
-            
-            // planes are always defined by x and z dimenions for horizontal plane detection, y is AWLAYS ZERO
-            // define new plan based on added anchor
-            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
-            
-            let planeNode = SCNNode()
-            planeNode.position = SCNVector3(planeAnchor.center.x, 0, planeAnchor.center.z)
-            // transfrom from standard x, y to x, z
-            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2, 1, 0, 0)
-            
-            let gridMaterial = SCNMaterial()
-            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
-            
-            plane.materials = [gridMaterial]
-            
-            planeNode.geometry = plane
-            
-            node.addChildNode(planeNode)
-        } else {
-            return
+    @IBAction func removeAllDice(_ sender: UIBarButtonItem) {
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                dice.removeFromParentNode()
+            }
         }
     }
+    
+    
+    //MARK: - ARSCNViewDelegateMethods
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        // guard statement to replace if... else
+        // try to downcast planeAnchor to ARPlaneAnchor
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        
+        let planeNode = createPlane(withPlaneAnchor: planeAnchor)
+        
+        node.addChildNode(planeNode)
+    }
+    
+    //MARK: - Plane Rendering Methods
+    
+    func createPlane(withPlaneAnchor planeAnchor: ARPlaneAnchor) -> SCNNode {
+        // planes are always defined by x and z dimenions for horizontal plane detection, y is AWLAYS ZERO
+        // define new plan based on added anchor
+        let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        
+        let planeNode = SCNNode()
+        planeNode.position = SCNVector3(planeAnchor.center.x, 0, planeAnchor.center.z)
+        // transfrom from standard x, y to x, z
+        planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2, 1, 0, 0)
+        
+        let gridMaterial = SCNMaterial()
+        gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+        
+        plane.materials = [gridMaterial]
+        
+        planeNode.geometry = plane
+        
+        return planeNode
+    }
 }
+
+
+
+
